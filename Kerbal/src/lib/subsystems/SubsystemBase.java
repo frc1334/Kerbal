46baee7b406b5f53e8815e2a685bb476c2bc6ca3
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2019-05-21 20:48:17 -0400

    Update SubsystemBase.java
     - Updated the SubsystemBase abstract superclass that every template subsystem will extend

--- Kerbal/src/lib/subsystems/SubsystemBase.java
+++ Kerbal/src/lib/subsystems/SubsystemBase.java
@@ -1,5 +1,38 @@
 
+/*
+ *  Abstract superclass that every template subsystem will extend
+ */
+
 package lib.subsystems;
 
-public class SubsystemBase {
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+import com.ctre.phoenix.motorcontrol.can.BaseMotorController;
+
+public abstract class SubsystemBase extends Subsystem {
+
+    // Array of ctre BaseMotorController objects that represent motor controllers
+    protected BaseMotorController[] MotorControllers;
+
+    /*
+     *  Default Constructor with a list of BaseMotorController objects
+     *  (e.g., ctre Talon SRX, Victor SPX)
+     */
+    public SubsystemBase (BaseMotorController[] MotorControllers) {
+        this.MotorController = MotorControllers;
+    }
+
+    /*
+     *  initDefaultCommand method that calls an overridable special 
+     *  on start method for initializations .etc (Polymorphism)
+     */
+    public final void initDefaultCommand () {
+        specificInit();
+    }
+
+    /*
+     *  Method stub that is overrided. Called by initDefaultCommand
+     */
+    public void specificInit() {    }
+
 }
